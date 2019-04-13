8e76338e47ed59d94f4a22a3c636e0cc8108b5bb
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-04-13 12:39:57 -0300

    create a virtual attribute to file

--- src/models/File.js
+++ src/models/File.js
@@ -10,7 +10,15 @@ const FileSchema = new mongoose.Schema({
     required: true
   }
 }, 
-  { timestamps: true}
-)
+  { 
+    timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
+);
+
+FileSchema.virtual('url').get(function() {
+  return `http://localhost:3001/files/${encodeURIComponent(this.path)}`;
+});
 
 module.exports = mongoose.model('File', FileSchema);
