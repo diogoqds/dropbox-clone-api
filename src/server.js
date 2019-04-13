9ab3eff6b33aef24ddfb84f313e3c39f96f94a9c
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-04-13 12:51:52 -0300

    config websockets

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const http = require('http');
 
 const app = express();
+const server = http.Server(app);
+
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  socket.on('connectRoom', box => {
+    socket.join(box);
+  });
+});
 
 mongoose.connect(
   'mongodb://localhost:27017/dropbox-clone',
@@ -11,13 +21,19 @@ mongoose.connect(
 );
 const port = process.env.PORT || 3001;
 
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname,'..','tmp')));
 
 app.use(require('./routes'));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server running on http://localhost:${port}`);
 });
 
